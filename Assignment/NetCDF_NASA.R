721edbb5b6faef6bd8cd77d9c03ad969bce184fe
Davide Delfino <davidepc2001@gmail.com>
2024-10-13 00:59:10 +0200

    Rscript

---
+++ Assignment/NetCDF_NASA.R
@@ -0,0 +1,19 @@
+#Commands to set the directory:
+getwd()
+path <- "C:/Users/Davide/Desktop/Alma Mater/SECOND YEAR/GIS/Assignment"
+setwd(path)
+
+#libraries
+library(sf)
+library(spData)
+library(tidyverse) 
+library(ggplot2)
+library(readxl)
+library(dplyr)
+library(viridis)
+
+# ------------------------------netCDF_NASA assignment -------------------------
+
+
+
+# ----
